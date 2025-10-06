29385c820ea36d4cb9b5eba1ea528edaa3665f22
cbowler8090-eng <cbowler8090@xmail.dixietech.edu>
2025-10-06 11:25:37 -0600

    inital app

---
+++ Quiz/Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Camden Bowler on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
